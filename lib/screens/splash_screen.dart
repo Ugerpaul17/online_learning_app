4f58368a7da71a30db2ebd8e72d071d141df43ef
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-18 13:10:20 +0300

    add school icon

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,8 +10,41 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(color: Colors.redAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.school_rounded,
+                          color: Colors.black,
+                          size: 50.0,
+
+                        ),
+                      ),
+                    ],
+
+                  ),
+
+              ),),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
