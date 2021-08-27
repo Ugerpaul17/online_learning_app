4b51297d6bb507c4e3b467486fa703f3dbe450fc
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-27 13:20:32 +0300

    create custom text file

---
+++ lib/components/custom_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  final String? text;
+  final double? size;
+  final Color? color;
+  final FontWeight? weight;
+  const CustomText({Key? key, this.color, this.size, this.text, this.weight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
