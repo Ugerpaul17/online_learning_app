0abe9f86ea48300285e6362aefe5744f722a542d
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-27 12:40:14 +0300

    create a stateless widget

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -1,1 +1,22 @@
 import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String? text;
+  final Function? press;
+  final Color? color, textColor;
+
+  const RoundedButton(
+      {Key? key,
+      this.text,
+      this.press,
+      this.color = Colors.redAccent,
+      this.textColor = Colors.white})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
