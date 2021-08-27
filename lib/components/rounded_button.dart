0429ea2fa9465e14b505b4cc97972cbd4acbe6ba
ugerpaul17 <ugerpaul5@gmail.com>
2021-08-27 14:53:11 +0300

    add inkwell widget for the botton

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:online_learning_app/components/custom_text.dart';
 
 class RoundedButton extends StatelessWidget {
-  final String? text;
-  final Function? press;
-  final Color? color, textColor;
+  final String? title;
+  final VoidCallback? onTap;
 
-  const RoundedButton(
-      {Key? key,
-      this.text,
-      this.press,
-      this.color = Colors.redAccent,
-      this.textColor = Colors.white})
-      : super(key: key);
+  const RoundedButton({
+    Key? key,
+    this.title,
+    this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            color: Colors.redAccent, borderRadius: BorderRadius.circular(15)),
+        alignment: Alignment.center,
+        width: double.maxFinite,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        child: CustomText(
+          text: title,
+          color: Colors.white,
+          size: 20.0,
+          weight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
